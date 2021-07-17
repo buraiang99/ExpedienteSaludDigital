17ca48cdddf43a492c06505972a7b6ccd0eaf75c
JorgeM81 <madrigal.jorge81@yahoo.es>
2021-07-16 18:01:17 -0600

    se agrego model e interface de paciente y se conecta al api

---
+++ app/src/main/java/cr/ac/ucr/expedientesaluddigital/models/Paciente.java
@@ -0,0 +1,67 @@
+package cr.ac.ucr.expedientesaluddigital.models;
+
+public class Paciente {
+    private String cedula;
+    private String nombre;
+    private int edad;
+    private String tipoSangre;
+    private String estadoCivil;
+    private String domicilio;
+    private String pass;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public void setTipoSangre(String tipoSangre) {
+        this.tipoSangre = tipoSangre;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
